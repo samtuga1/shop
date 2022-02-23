d74fe69e706535664bd54235c9d148748a58db6b
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-02-23 12:30:16 +0000

    Added my product_detail_screen.dart

---
+++ shop/lib/screens/product_detail_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/providers/products.dart';
+
+class ProductDetailScreen extends StatelessWidget {
+  static String id = '/product_detail_screen';
+  // final String title;
+  const ProductDetailScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final productId = ModalRoute.of(context)!.settings.arguments as String;
+    final loadedProduct =
+        Provider.of<Products>(context, listen: false).findById(productId);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(loadedProduct.title),
+      ),
+    );
+  }
+}
