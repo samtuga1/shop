3a450ac30c9c82208a08b865ef0e2abd30840c05
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-02-23 12:29:00 +0000

    Added my theming and the routes table

--- shop/lib/main.dart
+++ shop/lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import './screens/product_detail_screen.dart';
+import './screens/products_overview_screen.dart';
+import './providers/products.dart';
 
 void main() => runApp(const MyApp());
 
@@ -7,27 +11,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('MyShop'),
-      ),
-      body: const Center(
-        child: Text('Let\'s build a shop!'),
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(),
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato',
+        ),
+        home: const ProductsOverviewScreen(),
+        routes: {
+          ProductDetailScreen.id: (ctx) => const ProductDetailScreen(),
+        },
       ),
     );
   }
