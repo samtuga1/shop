d63c4399e68ca78f97325f4fe65a3db56299af4a
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-02-23 12:33:10 +0000

    Added the provider state management package into my app in the pubspec.yaml

--- shop/lib/screens/products_overview_screen.dart
+++ shop/lib/screens/products_overview_screen.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import '../widgets/products_grid.dart';
 
 class ProductsOverviewScreen extends StatelessWidget {
   const ProductsOverviewScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('MyShop'),
+      ),
+      body: const ProductsGrid(),
+    );
   }
 }
