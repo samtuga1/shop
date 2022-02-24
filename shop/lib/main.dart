adc07c82646a3df1526ae8970b644d15f499957a
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-02-24 14:06:41 +0000

    Set the debugBanner to false

--- shop/lib/main.dart
+++ shop/lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (ctx) => Products(),
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'MyShop',
         theme: ThemeData(
           primarySwatch: Colors.purple,
