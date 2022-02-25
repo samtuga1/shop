6ca45bc8610a19d4013867867d026338a5be6b03
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-02-25 13:41:22 +0000

    Implemented a cart_screen.dart

---
+++ shop/lib/providers/cart.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/foundation.dart';
+
+class CartItem {
+  final String id;
+  final String title;
+  final double price;
+  final int quantity;
+  CartItem({
+    required this.title,
+    required this.id,
+    required this.price,
+    required this.quantity,
+  });
+}
+
+class Cart with ChangeNotifier {
+  final Map<String, CartItem> _items = {};
+
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  int get itemCount {
+    return _items.length;
+  }
+
+  void addItem(String productId, String title, double price) {
+    if (_items.containsKey(productId)) {
+      _items.update(
+        productId,
+        (existingCartItem) => CartItem(
+          title: existingCartItem.title,
+          id: existingCartItem.id,
+          price: existingCartItem.price,
+          quantity: existingCartItem.quantity + 1,
+        ),
+      );
+    } else {
+      _items.putIfAbsent(
+        productId,
+        () => CartItem(
+            title: title,
+            id: DateTime.now().toString(),
+            price: price,
+            quantity: 1),
+      );
+    }
+    notifyListeners();
+  }
+}
