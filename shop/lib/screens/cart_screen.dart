6f43f88d378dad5df4d7a2fe4cab105d9fa8c070
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-02-26 09:44:51 +0000

    Implemented a cart_screen.dart

--- shop/lib/screens/cart_screen.dart
+++ shop/lib/screens/cart_screen.dart
@@ -1,1 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/providers/cart.dart';
+import 'package:shop/widgets/cart_item.dart' as ci;
+
+import '../providers/orders.dart';
+
+class CartScreen extends StatelessWidget {
+  static const routeName = '/cart';
+  const CartScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final cart = Provider.of<Cart>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cart Screen'),
+      ),
+      body: Column(
+        children: [
+          Card(
+            margin: const EdgeInsets.all(15),
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Total',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  const Spacer(),
+                  Chip(
+                    label: Text(
+                      '\$${cart.totalAmount}',
+                      style: TextStyle(
+                          color: Theme.of(context)
+                              .primaryTextTheme
+                              .titleMedium
+                              ?.color),
+                    ),
+                    backgroundColor: Theme.of(context).primaryColor,
+                  ),
+                  FlatButton(
+                    onPressed: () {
+                      Provider.of<Orders>(context, listen: false).addOrder(
+                          cart.totalAmount, cart.items.values.toList());
+                      cart.clear();
+                    },
+                    child: Text(
+                      'ORDER NOW',
+                      style: TextStyle(color: Theme.of(context).primaryColor),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: cart.items.length,
+              itemBuilder: (context, i) => ci.CartItem(
+                productId: cart.items.keys.toList()[i],
+                id: cart.items.values.toList()[i].id,
+                title: cart.items.values.toList()[i].title,
+                price: cart.items.values.toList()[i].price,
+                quantity: cart.items.values.toList()[i].quantity,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
