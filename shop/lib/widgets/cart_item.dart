47ba54b0d7f31ebf3415b9850178dfdf1ee1d48f
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-02-26 09:44:23 +0000

    Implemented a cart_item.dart

---
+++ shop/lib/widgets/cart_item.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:shop/providers/cart.dart';
+import 'package:provider/provider.dart';
+
+class CartItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String productId;
+  final double price;
+  final int quantity;
+  const CartItem(
+      {Key? key,
+      required this.id,
+      required this.title,
+      required this.price,
+      required this.quantity,
+      required this.productId})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Dismissible(
+      key: ValueKey(id),
+      background: Container(
+        alignment: Alignment.centerRight,
+        color: Theme.of(context).errorColor,
+        child: Container(
+          margin: const EdgeInsets.only(right: 20),
+          child: const Icon(
+            Icons.delete,
+            color: Colors.white,
+            size: 40,
+          ),
+        ),
+        margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+      ),
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction) {
+        Provider.of<Cart>(context, listen: false).removeItem(productId);
+      },
+      child: Card(
+        margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListTile(
+            leading: CircleAvatar(
+              child: Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: FittedBox(child: Text('\$$price')),
+              ),
+            ),
+            title: Text(title),
+            subtitle: Text('Total: \$${(price * quantity)}'),
+            trailing: Text('$quantity x'),
+          ),
+        ),
+      ),
+    );
+  }
+}
