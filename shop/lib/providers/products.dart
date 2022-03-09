e4e14058116597ca7f39805ffc5a24b74252e3b9
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-03-09 18:20:40 +0000

    Implemented an addProduct function

--- shop/lib/providers/products.dart
+++ shop/lib/providers/products.dart
@@ -48,4 +48,15 @@ class Products with ChangeNotifier {
   Product findById(id) {
     return items.firstWhere((prod) => prod.id == id);
   }
+
+  void addProduct(Product product) {
+    final newProduct = Product(
+        id: DateTime.now().toString(),
+        title: product.title,
+        description: product.description,
+        price: product.price,
+        imageUrl: product.imageUrl);
+    _items.add(newProduct);
+    notifyListeners();
+  }
 }
