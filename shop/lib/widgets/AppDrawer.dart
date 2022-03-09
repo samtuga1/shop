9c2df7ab734fd38b94dcf71ad909d2cc471b7a89
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-03-09 18:22:29 +0000

    Implemented a list tile

--- shop/lib/widgets/AppDrawer.dart
+++ shop/lib/widgets/AppDrawer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shop/screens/orders.screen.dart';
+import 'package:shop/screens/user_products_screen.dart';
 import '../screens/products_overview_screen.dart';
 
 class AppDrawer extends StatelessWidget {
@@ -32,6 +33,14 @@ class AppDrawer extends StatelessWidget {
             onTap: () {
               Navigator.of(context)
                   .pushReplacementNamed(OrdersScreen.routeName);
+            },
+          ),
+          const Divider(),
+          ListTile(
+            leading: const Icon(Icons.edit),
+            title: const Text('Manage Products'),
+            onTap: () {
+              Navigator.of(context).pushReplacementNamed(UserProductsScreen.id);
             },
           )
         ],
