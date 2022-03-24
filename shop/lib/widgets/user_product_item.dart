e2b28c5066b96e3d879c69abadf5952e53cd92b9
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-03-24 09:48:33 +0000

    Updated the constructor

--- shop/lib/widgets/user_product_item.dart
+++ shop/lib/widgets/user_product_item.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:shop/screens/edit_product_screen.dart';
+import 'package:provider/provider.dart';
+import '../providers/products.dart';
 
 class UserProductItem extends StatelessWidget {
   final String title;
   final String id;
   final String imageUrl;
-  const UserProductItem({Key? key, required this.title, required this.imageUrl, required this.id})
+  const UserProductItem(
+      {Key? key, required this.title, required this.imageUrl, required this.id})
       : super(key: key);
 
   @override
@@ -27,7 +30,10 @@ class UserProductItem extends StatelessWidget {
             color: Theme.of(context).primaryColor,
           ),
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              Provider.of<Products>(context, listen: false)
+                  .deleteSingleProduct(id);
+            },
             icon: const Icon(Icons.delete),
             color: Theme.of(context).errorColor,
           )
