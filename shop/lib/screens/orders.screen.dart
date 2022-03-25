12c2e00b0793d8294ea0e8c3a0c43cd3ee9fb029
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-03-25 23:10:34 +0000

    Updates

--- shop/lib/screens/orders.screen.dart
+++ shop/lib/screens/orders.screen.dart
@@ -4,9 +4,37 @@ import '../providers/orders.dart';
 import '../widgets/AppDrawer.dart';
 import '../widgets/order_item.dart' as ord;
 
-class OrdersScreen extends StatelessWidget {
+class OrdersScreen extends StatefulWidget {
   static const routeName = '/orders';
   const OrdersScreen({Key? key}) : super(key: key);
+
+  @override
+  State<OrdersScreen> createState() => _OrdersScreenState();
+}
+
+class _OrdersScreenState extends State<OrdersScreen> {
+  bool isInit = true;
+  bool isLoading = false;
+
+  Future<void> fetchData() async {
+    final orderData = Provider.of<Orders>(context, listen: false);
+    setState(() {
+      isLoading = true;
+    });
+    await orderData.fetchAndSet();
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (isInit) {
+      fetchData();
+    }
+    isInit = false;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +44,7 @@ class OrdersScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Your Orders'),
       ),
-      body: ListView.builder(
+      body: isLoading ? const Center(child: CircularProgressIndicator(),) : ListView.builder(
         itemCount: orderData.orders.length,
         itemBuilder: (ctx, i) => ord.OrderItem(
           order: orderData.orders[i],
