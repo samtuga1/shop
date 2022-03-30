e574d8aac63346c557e067ad1dbd0f03f80678a2
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-03-30 18:33:23 +0000

    Update

--- shop/lib/providers/products.dart
+++ shop/lib/providers/products.dart
@@ -5,47 +5,19 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class Products with ChangeNotifier {
-  List<Product> _items = [
-    // Product(
-    //   id: 'p1',
-    //   title: 'Red Shirt',
-    //   description: 'A red shirt - it is pretty red!',
-    //   price: 29.99,
-    //   imageUrl:
-    //       'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
-    // ),
-    // Product(
-    //   id: 'p2',
-    //   title: 'Trousers',
-    //   description: 'A nice pair of trousers.',
-    //   price: 59.99,
-    //   imageUrl:
-    //       'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
-    // ),
-    // Product(
-    //   id: 'p3',
-    //   title: 'Yellow Scarf',
-    //   description: 'Warm and cozy - exactly what you need for the winter.',
-    //   price: 19.99,
-    //   imageUrl:
-    //       'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
-    // ),
-    // Product(
-    //   id: 'p4',
-    //   title: 'A Pan',
-    //   description: 'Prepare any meal you want.',
-    //   price: 49.99,
-    //   imageUrl:
-    //       'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
-    // ),
-  ];
+  List<Product>? _items = [];
+
+  String? authToken;
+
+  Products(this.authToken, this._items);
+  Products.diffConstructor();
 
   List<Product> get favoriteItems {
-    return _items.where((prodItem) => prodItem.isFavorite).toList();
+    return _items!.where((prodItem) => prodItem.isFavorite).toList();
   }
 
   List<Product> get items {
-    return [..._items];
+    return [..._items!];
   }
 
   Product findById(id) {
@@ -53,10 +25,12 @@ class Products with ChangeNotifier {
   }
 
   Future<void> getAndSetProducts() async {
-    const url = 'https://shop-12901-default-rtdb.firebaseio.com/products.json';
+    final url =
+        'https://shop-12901-default-rtdb.firebaseio.com/products.json/?auth=$authToken';
     try {
       final response = await http.get(Uri.parse(url));
-      var extractedResponse = json.decode(response.body) as Map<String, dynamic>;
+      var extractedResponse =
+          json.decode(response.body) as Map<String, dynamic>;
       List<Product> loadedProducts = [];
       extractedResponse.forEach((prodId, product) {
         loadedProducts.add(
@@ -100,7 +74,7 @@ class Products with ChangeNotifier {
         imageUrl: product.imageUrl,
       );
       print(json.decode(response.body)['name']);
-      _items.add(newProduct);
+      _items!.add(newProduct);
       notifyListeners();
     } catch (error) {
       print(error);
@@ -109,7 +83,7 @@ class Products with ChangeNotifier {
   }
 
   Future<void> editSingleProduct(String? id, Product product) async {
-    int prodIndex = _items.indexWhere((prod) => prod.id == id);
+    int prodIndex = _items!.indexWhere((prod) => prod.id == id);
     if (prodIndex >= 0) {
       final url =
           'https://shop-12901-default-rtdb.firebaseio.com/products/$id.json';
@@ -124,7 +98,7 @@ class Products with ChangeNotifier {
           },
         ),
       );
-      _items[prodIndex] = product;
+      _items![prodIndex] = product;
       notifyListeners();
     } else {
       print('...');
@@ -135,13 +109,13 @@ class Products with ChangeNotifier {
     final url =
         'https://shop-12901-default-rtdb.firebaseio.com/products/$id.json';
     final existingProductIndex =
-        _items.indexWhere((product) => product.id == id);
-    Product? existingProduct = _items[existingProductIndex];
-    _items.removeWhere((product) => product.id == id);
+        _items!.indexWhere((product) => product.id == id);
+    Product? existingProduct = _items![existingProductIndex];
+    _items!.removeWhere((product) => product.id == id);
     notifyListeners();
     final response = await http.delete(Uri.parse(url));
     if (response.statusCode >= 400) {
-      _items.insert(existingProductIndex, existingProduct);
+      _items!.insert(existingProductIndex, existingProduct);
       notifyListeners();
       throw HttpException('Was not able to delete');
     }
